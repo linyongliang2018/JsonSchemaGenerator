b109f3eeaea71183c783783c0d2eb7fa7ceda87b
GitHub Action <action@github.com>
2023-04-16 06:04:19 +0000

    Template cleanup

--- src/main/kotlin/com/github/linyongliang2018/jsonschemagenerator/listeners/MyFrameStateListener.kt
+++ src/main/kotlin/com/github/linyongliang2018/jsonschemagenerator/listeners/MyFrameStateListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.linyongliang2018.jsonschemagenerator.listeners
 
 import com.intellij.ide.FrameStateListener
 import com.intellij.openapi.diagnostic.thisLogger
